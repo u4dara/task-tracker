b8e831788ce91b0b039d23406a5d4848eca0d4c8
Udara <u4dara@protonmail.com>
2025-08-28 12:37:03 +0530

    auth routes done

--- server/configs/env.js
+++ server/configs/env.js
@@ -2,4 +2,5 @@ import { config } from 'dotenv';
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const { NODE_ENV, PORT, MONGODB_URI } = process.env;
+export const { NODE_ENV, PORT, MONGODB_URI, JWT_SECRET, JWT_EXPIRES_IN } =
+   process.env;
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,18 +1,106 @@
 import asyncHandler from 'express-async-handler';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
-// @des	   SIGNUP a new User
+import User from '../models/user.model.js';
+import { JWT_SECRET, JWT_EXPIRES_IN } from '../configs/env.js';
+
+// JWT Token Generation
+const generateToken = (id) => {
+   return jwt.sign({ userId: id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+   });
+};
+
+// @des	   SIGN-UP a new User
 // @route   POST /api/v1/auth/sign-up
 export const signUp = asyncHandler(async (req, res) => {
-   res.status(201).json({ success: true, data: 'user created successfully' });
+   const session = await mongoose.startSession();
+   await session.startTransaction();
+
+   const { name, email, password } = req.body;
+
+   if (!name || !email || !password) {
+      const error = new Error('Please enter all fields!');
+      error.statusCode = 400;
+      throw error;
+   }
+
+   const isUserExists = await User.findOne({ email });
+
+   if (isUserExists) {
+      const error = new Error('User already exists!');
+      error.statusCode = 401;
+      throw error;
+   }
+   // Hash the Password
+   const salt = await bcrypt.genSalt(10);
+   const harshedPassword = await bcrypt.hash(password, salt);
+
+   const newUser = await User.create(
+      [
+         {
+            name,
+            email,
+            password: harshedPassword,
+         },
+      ],
+
+      { session }
+   );
+
+   const token = generateToken(newUser[0]._id);
+
+   await session.commitTransaction();
+   session.endSession();
+   res.status(201).json({
+      success: true,
+      message: 'User created successfully!',
+      data: { token, user: newUser },
+   });
 });
 
-// @des	   SIGNIN a User
+// @des	   SIGN-IN a User
 // @route   POST /api/v1/auth/sign-in
 export const signIn = asyncHandler(async (req, res) => {
-   res.status(200).json({ success: true, data: 'user signed-in successfully' });
+   const { email, password } = req.body;
+
+   if (!email || !password) {
+      const error = new Error('Please enter all fields!');
+      error.statusCode = 400;
+      throw error;
+   }
+
+   const user = await User.findOne({ email });
+
+   if (!user) {
+      const error = new Error('User does not exists!');
+      error.statusCode = 404;
+      throw error;
+   }
+
+   const isPasswordValid = await bcrypt.compare(password, user.password);
+
+   if (!isPasswordValid) {
+      const error = new Error('Invalid Password!');
+      error.statusCode = 401;
+      throw error;
+   }
+
+   const token = generateToken(user._id);
+
+   res.status(200).json({
+      success: true,
+      message: 'User successfully logged in!',
+      data: {
+         token,
+         user,
+      },
+   });
 });
 
-// @des	   SIGNOUT a User
+// @des	   SIGN-OUT a User
 // @route   POST /api/v1/auth/sign-out
 export const signOut = asyncHandler(async (req, res) => {
    res.status(200).json({
